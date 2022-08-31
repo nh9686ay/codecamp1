557c729332b9c89df88f31d10b1c67773dca9c5c
Emon <emonmnsu95@gmail.com>
2022-08-31 10:41:13 -0500

    jsx file and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 function App() {
   return (
-    <div className="App">
-      <main>Hello everyone</main>
-
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
